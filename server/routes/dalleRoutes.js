e571149f749b9e2e29321889bb2cc13f4f903ada
PRIYANKAec <priyankaa261103@gmail.com>
2024-07-17 19:09:52 +0530

    Implemented gnerating images through the openai and cLoudinary setup

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -13,7 +13,7 @@ const configuration= new Configuration({
 const openai = new OpenAIApi(configuration);
 
 router.route('/').get((req,res) => {
-    res.send('Hello DALL-E!');
+    res.send('Hello from DALL-E!');
 });
 
 router.route('/').post(async(req, res) => {
@@ -21,6 +21,7 @@ router.route('/').post(async(req, res) => {
         const { prompt } = req.body;
 
         const aiResponse = await openai.createImage({
+            model: "dall-e-2",
             prompt,
             n: 1,
             size: '1024x1024',
@@ -29,6 +30,8 @@ router.route('/').post(async(req, res) => {
 
         const image = aiResponse.data.data[0].b64_json;
 
+        res.status(200).json({ photo: image});
+        
     } catch(error) {
         console.log(error);
         res.status(500).send(error?.response.data.error.message)
